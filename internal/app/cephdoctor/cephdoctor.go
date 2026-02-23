6b4580039e166e418f70a85a0e9519537400f687
biosvos <biosvos@gmail.com>
2026-02-23 18:09:18 +0900

    feat: cluster CLI를 repository 기반으로 연동

--- internal/app/cephdoctor/cephdoctor.go
+++ internal/app/cephdoctor/cephdoctor.go
@@ -1,12 +1,17 @@
 package cephdoctor
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 
 	"github.com/alecthomas/kong"
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/neatflowcv/ceph-doctor/internal/domain"
+	"github.com/neatflowcv/ceph-doctor/internal/infrastructure/fscluster"
 )
 
 type cli struct {
@@ -37,36 +42,78 @@ func (e exitCodeError) Error() string { return "" }
 
 func (e exitCodeError) ExitCode() int { return int(e) }
 
-func (c *clusterRegisterCmd) Run() error {
+func (c *clusterRegisterCmd) Run(repo domain.ClusterRepository) error {
 	slog.Info("cluster register", "name", c.Name, "host", c.Host)
 
-	return exitCodeError(1)
+	cluster, err := domain.NewCluster(c.Name, c.Key, []string{c.Host})
+	if err != nil {
+		return fmt.Errorf("new cluster: %w", err)
+	}
+
+	err = repo.CreateCluster(context.Background(), cluster)
+	if errors.Is(err, domain.ErrClusterAlreadyExists) {
+		return exitCodeError(1)
+	}
+
+	if err != nil {
+		return fmt.Errorf("create cluster: %w", err)
+	}
+
+	return nil
 }
 
-func (c *clusterUnregisterCmd) Run() error {
+func (c *clusterUnregisterCmd) Run(repo domain.ClusterRepository) error {
 	slog.Info("cluster unregister", "name", c.Name)
 
-	return exitCodeError(1)
+	err := repo.DeleteCluster(context.Background(), c.Name)
+	if errors.Is(err, domain.ErrClusterNotFound) {
+		return exitCodeError(1)
+	}
+
+	if err != nil {
+		return fmt.Errorf("delete cluster: %w", err)
+	}
+
+	return nil
 }
 
-func (c *clusterListCmd) Run() error {
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Name", "Host"})
-	t.Render()
+func (c *clusterListCmd) Run(repo domain.ClusterRepository) error {
+	clusters, err := repo.ListClusters(context.Background())
+	if err != nil {
+		return fmt.Errorf("list clusters: %w", err)
+	}
 
-	return exitCodeError(1)
+	tableWriter := table.NewWriter()
+	tableWriter.SetOutputMirror(os.Stdout)
+	tableWriter.AppendHeader(table.Row{"Name", "Hosts"})
+
+	for _, cluster := range clusters {
+		tableWriter.AppendRow(table.Row{
+			cluster.Name(),
+			strings.Join(cluster.Hosts(), ","),
+		})
+	}
+
+	tableWriter.Render()
+
+	return nil
 }
 
 func Execute() error {
-	var command cli
-
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, nil)))
+
+	repo, err := fscluster.NewRepository("")
+	if err != nil {
+		return fmt.Errorf("new repository: %w", err)
+	}
+
+	var command cli
 
 	parser, err := kong.New(
 		&command,
 		kong.Name("cephdoctor"),
 		kong.Description("Ceph Doctor CLI"),
+		kong.BindTo(repo, (*domain.ClusterRepository)(nil)),
 	)
 	if err != nil {
 		return fmt.Errorf("create parser: %w", err)
